f75e5a73133b702f8c80d80946c34448ca9073c4
Raphael <naduma.raphael4@gmail.com>
2025-01-20 21:30:37 +0800

    cubic bezier

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,12 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      transitionTimingFunction: {
+        "in-expo": "cubic-bezier(0.1, 2.7, 0.98, 1)",
+        "out-expo": "cubic-bezier(0.19, 1, 0.22, 1)",
+      },
+    },
   },
   plugins: [],
 };
