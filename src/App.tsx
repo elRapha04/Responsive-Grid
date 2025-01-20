8436300bbe9ed1d592139b1922e3b3d3c83d123d
Raphael <naduma.raphael4@gmail.com>
2025-01-20 21:45:45 +0800

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ const App = () => {
   return (
     <div
       id="full-window"
-      className="w-full h-screen flex flex-col justify-center items-center bg-gradient-to-bl from-yellow-400 via-pink to-purple-500"
+      className="w-full h-screen bg-cover bg-no-repeat flex flex-col justify-center items-center bg-gradient-to-bl from-yellow-400 via-pink to-purple-500"
     >
       <h1 className="font-sans font-mono font-semibold italic text-2xl sm:text-3xl md:text-4xl lg:text-6xl mb-10">
         Beginner Programming Activity: Build a Creative Grid with React and
