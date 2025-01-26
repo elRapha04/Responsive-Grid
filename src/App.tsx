d197a94001be0eb448777a33657f56b963ac1b94
Raphael <naduma.raphael4@gmail.com>
2025-01-26 22:26:40 +0800

    rmv font-mono in apptsx

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ const App = () => {
       id="full-window"
       className="absolute inset-0 p-4 w-full h-full bg-cover bg-no-repeat flex flex-col justify-center items-center bg-gradient-to-bl from-yellow-400 via-pink to-purple-500"
     >
-      <h1 className="font-sans font-mono font-semibold italic text-2xl sm:text-3xl md:text-4xl mb-10">
+      <h1 className="font-sans font-semibold italic text-2xl sm:text-3xl md:text-4xl mb-10">
         Beginner Programming Activity: Build a Creative Grid with React and
         Tailwind CSS!
       </h1>
